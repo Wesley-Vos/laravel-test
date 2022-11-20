44d39d724662fd47a2e6835fd5a1479fa4735262
Wesley Vos <wesley.vos@icloud.com>
2022-11-20 15:50:14 +0100

    Move all models to Models folder

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Product;
-use App\User;
-use Illuminate\Http\Request;
+use App\Models\Product;
 use App\Services\ProductService;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
